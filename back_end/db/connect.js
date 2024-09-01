325080b9c4ab15686f3b39df5e136269ea3ad1bb
Violet005-sys <violetwarutere@gmail.com>
2024-09-01 22:56:34 +0300

    atlas update

--- back_end/db/connect.js
+++ back_end/db/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const connectionString = "mongodb+srv://violetwarutere:mongoDB2024@violet-atlas.ublwfkn.mongodb.net/?retryWrites=true&w=majority&appName=Violet-Atlas/Bookly";
+const connectionString = "mongodb+srv://violetwarutere:mongoDB2024@violet-atlas.ublwfkn.mongodb.net/Bookly?retryWrites=true&w=majority&appName=Violet-Atlas";
 
 const connect_db = async () => {
   
